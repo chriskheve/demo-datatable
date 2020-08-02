d62d26d66243e7c1d94d55c713c727caedc835e7
Chris Kheve <chriskheve@gmail.com>
2020-08-02 08:51:08 +0200

    Fix typo

--- database/migrations/2020_08_02_034700_create_role_user_table.php
+++ database/migrations/2020_08_02_034700_create_role_user_table.php
@@ -15,8 +15,8 @@ class CreateRoleUserTable extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
-            $table->integer('role_id')->integer();
-            $table->integer('user_id')->integer();
+            $table->integer('role_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
         });
     }
